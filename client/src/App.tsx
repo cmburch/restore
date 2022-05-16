3646194202a7c72d10c133c18d6fb551eda9d3e5
charles burch <charles_burch89@yahoo.com>
2022-05-16 22:38:56 +0800

    [example] using setstate method to add products to list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,16 @@ function App() {
     { name: "product2", price: 200 },
   ]);
 
+  function addProduct(){
+    setProduct((prevState) => [
+      ...prevState,
+      {
+        name: `product${prevState.length + 1}`,
+        price: prevState.length * 100 + 100,
+      },
+    ]);
+  }
+
   return (
     <div>
       <h1 style={{ color: "red" }}>Re-Store</h1>
@@ -14,6 +24,7 @@ function App() {
           <li key={ index }>{product.name} - {product.price}</li>
         )) }
       </ul>
+      <button onClick={ addProduct }> Add Product</button>
     </div>
   );
 }
