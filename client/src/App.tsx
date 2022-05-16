e4b6309b20604241975cc846318ebd72808abffa
charles burch <charles_burch89@yahoo.com>
2022-05-16 22:30:48 +0800

    [example] using map method to display list of products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 function App() {
+  const [products, setProduct] = useState([
+    { name: "product1", price: 100 },
+    { name: "product2", price: 200 },
+  ]);
+
   return (
     <div>
-      <h1 style={{color: 'red'}}>Re-Store</h1>
+      <h1 style={{ color: "red" }}>Re-Store</h1>
+      <ul>
+        { products.map((product,index)=> (
+          <li key={ index }>{product.name} - {product.price}</li>
+        )) }
+      </ul>
     </div>
   );
 }
